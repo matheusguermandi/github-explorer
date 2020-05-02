445e342cc1007db62a523ea53153a0d95d274e7c
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-05-02 14:57:24 -0300

    Atualizando as rotas e páginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact />
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository/:repository+" component={Repository} />
   </Switch>
 );
 
