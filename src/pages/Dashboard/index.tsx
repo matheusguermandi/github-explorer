9ea0afa6ba2579b12e49a7ec4088b0d254c055bb
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-05-02 15:59:28 -0300

    Construindo e Estilizando página Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="" alt="" />
+
+          <div>
+            <strong>Teste</strong>
+            <p>Teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
